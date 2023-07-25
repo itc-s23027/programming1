19a742fba5a31d91ad10e2b5fdbf95602a92dfa5
motohaya <s23027@std.it-college.ac.jp>
2023-07-25 05:34:54 +0000

    CHAPTER03/

---
+++ CHAPTER03/Q3_8_1.py
@@ -0,0 +1,7 @@
+import pickle
+
+with open('test1.pkl', 'wb') as f:
+    my_list = list(range(1, 11))
+    pickle.dump(my_list, f)
+
+
--- CHAPTER03/Q3_8_2.py
+++ CHAPTER03/Q3_8_2.py
@@ -1,5 +1,9 @@
 import pickle
 
 with open('test1.pkl', 'rb') as f:
-	result = pickle.load(f)
-	print(result)
+    result = pickle.load(f)
+    print(result)
+
+
+
+
